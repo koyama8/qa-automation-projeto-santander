3219dd7b0d3a2d28b3ae0777db79629012057b16
Matheus Koyama <koyamaCRS@gmail.com>
2026-07-16 16:29:05 -0300

    chore(api): configura framework Cypress para testes de API

---
+++ apps/api/cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+
+const {
+  addCucumberPreprocessorPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor')
+
+const {
+  createEsbuildPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor/esbuild')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://api.trello.com',
+    specPattern: 'cypress/e2e/**/*.feature',
+
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config)
+
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      )
+
+      return config
+    },
+  },
+})
